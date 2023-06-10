3e485b5d4ffe50358f6abae16eff41aa52a8dd6f
He Ming <68702601+ChaosFlameme@users.noreply.github.com>
2023-06-10 19:27:02 +0200

    Small fix on map

--- windowParameters.py
+++ windowParameters.py
@@ -2,15 +2,15 @@ class WindowParameter:
     #Unit interface size
     originTileSize = 16
     #A changer par player apres
-    SCALE = 4
+    SCALE = 2
 
     tileSize= originTileSize*SCALE
 
-    screenTileCol = 16
-    screenTileRow = 9
+    screenTileCol = 16*2
+    screenTileRow = 9*2
     
-    mapTileRow = 12
-    mapTileCol = 9
+    mapTileRow = 12*2
+    mapTileCol = 9*2
 
     screenWidth = tileSize * screenTileCol
     screenHeight = tileSize * screenTileRow
