254e1eba5ba51f2c34b986cdc5c5c82be6c73ebe
Karim Elmestari <karimelmestari@gmail.com>
2023-06-11 16:29:55 +0200

    map with sprites (working)

--- map.py
+++ map.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import random
+from PIL import Image,ImageTk
+from tkinter.constants import *
 
 
 from windowParameters import WindowParameter
@@ -15,44 +17,57 @@ class Map:
         self.spawnY = 0
 
     def generateMap(self, areaPlay):
-        map_width = WindowParameter.mapWidth
-        map_height = WindowParameter.mapHeight
+        self.map_width = WindowParameter.mapWidth
+        self.map_height = WindowParameter.mapHeight
         case_size = WindowParameter.tileSize
-        x_tile = map_width // case_size
-        y_tile = map_height // case_size
+        x_tile = self.map_width // case_size
+        y_tile = self.map_height // case_size
         print(x_tile)
         print(y_tile)
+        wall_image = Image.open(".\sprites\wall_mid.png").convert("RGBA")
+        wall_image = wall_image.resize((case_size, case_size), Image.ANTIALIAS)
+        self.wall_photo = ImageTk.PhotoImage(wall_image)
 
+        floor_image = Image.open("./sprites/floor_1.png").convert("RGBA")
+        floor_image= floor_image.resize((case_size, case_size), Image.ANTIALIAS)
+        self.floor_photo = ImageTk.PhotoImage(floor_image)
+        
+        
         # Dessine les cases
         for x in range(x_tile):
             for y in range(y_tile):
-                if x == x_tile - 1 and y == y_tile - 1 :
-                     # Coin inferieur droit (fin du niveau)
-                     fill_color = "red"
-                elif x == 0 and y == 0:
-                     # Coin superieur gauche (entrée du donjon)
-                     fill_color = "green"
-        for x in range(self.map_width // case_size):
-            for y in range(self.map_height // case_size):
                 if random.random() < 0.335:  # Changer la probabilité selon vos besoins
-                    fill_color = "black"  # Mur
-                else:
-                    fill_color = "white"  # Libre passage
-                x1 = x * case_size
-                y1 = y * case_size
-                x2 = x1 + case_size
-                y2 = y1 + case_size
-                if fill_color == "black":
-                        self.CaseNoire[self.indexDico] = [x1, y1, x2, y2]
-                        self.centreCaseNoire[self.indexDico] = [(x1+x2)/2, (y1+y2)/2]
-                        self.indexDico += 1
-                areaPlay.create_rectangle(x1, y1, x2, y2, fill=fill_color)
+                    areaPlay.create_image(x * case_size, y * case_size, anchor="nw", image=self.wall_photo)
+                    self.CaseNoire[self.indexDico] = [x * case_size, y * case_size, (x + 1) * case_size, (y + 1) * case_size]
+                    self.centreCaseNoire[self.indexDico] = [(x * case_size + (x + 1) * case_size) / 2, (y * case_size + (y + 1) * case_size) / 2]
+                    self.indexDico += 1
+                else:  
+                    areaPlay.create_image(x * case_size, y * case_size, anchor="nw", image=self.floor_photo)
+                # if fill_color == "black":
+                         
+                        
+                #if(fill_color== "black"):
+                    # # Create the canvas, size in pixels.
+                    # canvas = tk.Canvas(width=16, height=16)
+                    # # Pack the canvas into the Frame.
+                    # canvas.pack(expand=YES, fill=BOTH)
+                    # # Load the .gif image file.
+                    # wall = ImageTk.PhotoImage(file='./sprites/wall_mid.png')
+                    # # Put gif image on canvas.
+                    # # Pic's upper-left corner (NW) on the canvas is at x=50 y=10.
+                    # canvas.create_image(x, y, image=wall, anchor=NW)
+
+                    #wall= ImageTk.PhotoImage(Image.open("./sprites/wall_mid.png"))
+                    #wall = tk.PhotoImage(file = './sprites/wall_mid.png')
+                    #areaPlay.create_image(x1,y1, image= wall)
+                    #areaPlay.create_image(10,10,image=wall)    
         xRed = random.randint(0, (self.map_width-50)// case_size)
         yRed = random.randint(0, (self.map_height-50)// case_size)
         self.x1R = xRed * case_size
         self.y1R = yRed * case_size
         self.x2R = self.x1R + case_size
         self.y2R = self.y1R + case_size
+        
         areaPlay.create_rectangle(self.x1R, self.y1R, self.x2R, self.y2R, fill="red")
 
         self.xGreen = random.randint(0, (self.map_width-50)// case_size)
@@ -80,6 +95,8 @@ class Map:
             self.centreCaseNoire.pop(KeyfindR)
         self.spawnX = x1G + (case_size - 10) // 2
         self.spawnY = y1G + (case_size - 10) // 2
+
+
 
 
     def generateKey(self,areaPlay):
